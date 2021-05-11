060a68a58cd61534186af6d06349790ccb747140
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-11 07:40:31 -0500

    Agregamos los campos de la tabla Post; Creación del factory en Post; Agregamos usuario en el Seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        App\User::create([
+            'name' => 'Luis Alberto Ruiz Sarmiento',
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123123')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
