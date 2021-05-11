d1b557dc9f6834ec573845fc082bcbae9f898992
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-11 08:04:43 -0500

    Iniciamos la funcionalidad para mostrar el listado de Posts y ver el Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PAgeController@posts');
+Route::get('blog/{post}', 'PAgeController@post')->name('post');
 
 Auth::routes();
 
